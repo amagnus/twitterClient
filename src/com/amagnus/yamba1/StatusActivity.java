11a95d9b86898d5a2031966777b6b19b8a00115a
Adrien Magnus <adrien.magnus@gmail.com>
2012-08-04 08:30:56 -0700

    2nd commit

---
+++ src/com/amagnus/yamba1/StatusActivity.java
@@ -0,0 +1,20 @@
+package com.amagnus.yamba1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class StatusActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_status, menu);
+        return true;
+    }
+}
