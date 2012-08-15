c2615f6a26f1c52ce30d7e84f7fa822050256a5e
Adrien Magnus <adrien.magnus@gmail.com>
2012-08-14 23:20:47 -0700

    client posting to twitter

--- gen/com/amagnus/yamba1/R.java
+++ gen/com/amagnus/yamba1/R.java
@@ -11,30 +11,43 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int background=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int buttonUpdate=0x7f070001;
-        public static final int editText=0x7f070000;
-        public static final int menu_settings=0x7f070002;
+        public static final int buttonUpdate=0x7f080002;
+        public static final int editText=0x7f080001;
+        public static final int menu_settings=0x7f080003;
+        public static final int textCount=0x7f080000;
     }
     public static final class layout {
         public static final int activity_status=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_status=0x7f060000;
+        public static final int activity_status=0x7f070000;
+        public static final int menu=0x7f070001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int buttonUpdate=0x7f040006;
-        public static final int hello_world=0x7f040001;
-        public static final int hintText=0x7f040005;
-        public static final int menu_settings=0x7f040002;
-        public static final int titleStatus=0x7f040004;
-        public static final int title_activity_status=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int buttonUpdate=0x7f050006;
+        public static final int hello_world=0x7f050001;
+        public static final int hintText=0x7f050005;
+        public static final int menu_settings=0x7f050002;
+        public static final int summaryApiRoot=0x7f05000c;
+        public static final int summaryPassword=0x7f05000a;
+        public static final int summaryUsername=0x7f050008;
+        public static final int titleApiRoot=0x7f05000b;
+        public static final int titlePassword=0x7f050009;
+        public static final int titlePrefsActivity=0x7f05000d;
+        public static final int titleStatus=0x7f050004;
+        public static final int titleUsername=0x7f050007;
+        public static final int title_activity_status=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
+    }
+    public static final class xml {
+        public static final int prefs=0x7f040000;
     }
 }
--- src/com/amagnus/yamba1/StatusActivity.java
+++ src/com/amagnus/yamba1/StatusActivity.java
@@ -1,20 +1,97 @@
 package com.amagnus.yamba1;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.graphics.Color;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 
-public class StatusActivity extends Activity {
-
+public class StatusActivity extends Activity implements OnClickListener, TextWatcher {
+	
+	private static final String TAG = "StatusActivity";
+	EditText editText;
+	Button updateButton;
+	Twitter twitter;
+	TextView textCount;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+        
+        // Find views
+        editText = (EditText) findViewById(R.id.editText);
+        updateButton = (Button) findViewById(R.id.buttonUpdate);
+        updateButton.setOnClickListener(this);
+        
+        textCount = (TextView) findViewById(R.id.textCount);
+        textCount.setText(Integer.toString(140));
+        textCount.setTextColor(Color.GREEN);
+        editText.addTextChangedListener(this);
+        
+        twitter = new Twitter("amagnus", "blabla59");
+        twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+    }
+    
+    // Called when button is clicked
+    public void onClick(View v) {
+    	String status = editText.getText().toString();
+    	new PostToTwitter().execute(status);
+    	Log.d(TAG, "onClicked");
+    }
+    
+    // Asynchronously posts to twitter
+    class PostToTwitter extends AsyncTask<String, Integer, String> {
+    
+    	// Called to initiate the background activity
+    	@Override
+    	protected String doInBackground(String... statuses) {
+    		try {
+    			Twitter.Status status = twitter.updateStatus(statuses[0]); return status.text;
+    		} catch (TwitterException e) { Log.e(TAG, e.toString()); e.printStackTrace();
+    		return "Failed to post";
+    		} 
+    	}
+    	
+    	// Called when there's a status to be updated
+    	@Override
+    	protected void onProgressUpdate(Integer... values) {
+    		super.onProgressUpdate(values);
+    		// Not used in this case
+    	}
+    		
+    	// Called once the background activity has completed
+    	@Override
+    	protected void onPostExecute(String result) {
+    		Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+    	}
+    }
+    
+    // TextWatcher methods
+    public void afterTextChanged(Editable statusText) { //
+    	int count = 140 - statusText.length();
+    	textCount.setText(Integer.toString(count));
+    	textCount.setTextColor(Color.GREEN);
+    	if (count < 10)
+    		textCount.setTextColor(Color.YELLOW);
+    	if (count < 0)
+    		textCount.setTextColor(Color.RED);
+    }
+    
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+    
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_status, menu);
-        return true;
-    }
 }
