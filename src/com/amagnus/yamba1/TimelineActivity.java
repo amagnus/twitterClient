785b4c60a208597db22c3841c2266494c7ea6282
Adrien Magnus <adrien.magnus@gmail.com>
2012-09-05 07:23:20 -0700

    TimelineActivity is ready with new timestamp

--- src/com/amagnus/yamba1/TimelineActivity.java
+++ src/com/amagnus/yamba1/TimelineActivity.java
@@ -1,9 +1,13 @@
 package com.amagnus.yamba1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.SimpleCursorAdapter.ViewBinder;
+import android.text.format.DateUtils;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -14,9 +18,7 @@ public class TimelineActivity extends Activity {
 	SQLiteDatabase db;
 	Cursor cursor;
 	ListView listTimeline;
-	SimpleCursorAdapter adapter;
-	static final String[] FROM = { DbHelper.C_CREATED_AT, DbHelper.C_USER, DbHelper.C_TEXT };
-	static final int[] TO = { R.id.textCreatedAt, R.id.textUser, R.id.textText };
+	TimelineAdapter adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,12 @@ public class TimelineActivity extends Activity {
 		// Connect to database
 		dbHelper = new DbHelper(this); //
 		db = dbHelper.getReadableDatabase(); //
+		
+		// Check whether preferences have been set
+		if (yamba1.getPrefs().getString("username", null) == null) {
+			startActivity(new Intent(this, PrefsActivity.class));
+			Toast.makeText(this, R.string.msgSetupPrefs, Toast.LENGTH_LONG).show();
+		}
 	}
 	
 	@Override
@@ -47,9 +55,25 @@ public class TimelineActivity extends Activity {
 		cursor = db.query(DbHelper.TABLE, null, null, null, null, null, DbHelper.C_CREATED_AT + " DESC");
 		startManagingCursor(cursor);
 	
-		// Set up the adapter
-		adapter = new SimpleCursorAdapter(this, R.layout.row, cursor, FROM, TO);
+		// Create the adapter
+		adapter = new TimelineAdapter(this, cursor);
 		listTimeline.setAdapter(adapter);
+		adapter.setViewBinder(VIEW_BINDER);
 	}
+	
+	// View binder constant to inject business logic that converts a timestamp to relative time
+	static final ViewBinder VIEW_BINDER = new ViewBinder() {
+		
+		public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+			if (view.getId() != R.id.textCreatedAt)
+				return false;
+			
+			// Update the created at text to relative time
+			long timestamp = cursor.getLong(columnIndex);
+			CharSequence relTime = DateUtils.getRelativeTimeSpanString(view.getContext(), timestamp);
+			((TextView) view).setText(relTime);
+			return true;
+		}	
+	};
 	
 }
